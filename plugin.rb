bb04050e01ce6fc032944290874be21a52bdafb1
Unicorn9x <unicorn9x@yahoo.com.vn>
2024-10-23 22:11:27 +0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,14 +5,16 @@
 # version: 0.1
 # authors: Unicorn9x
 
+enabled_site_setting :custom_bbcode_enabled
+
 after_initialize do
   # Register the custom BBCode
   Discourse::Markup.register(:bbcode, 'hide-for-guests') do |content|
     "<div class='hide-for-guests'>#{content}</div>"
   end
-
-  # Ensure the custom BBCode is parsed
-  add_to_serializer(:post, :hide_for_guests) do
+  
+  # Modify the post serializer to recognize the custom BBCode
+  add_to_serializer(:post, :custom_bbcode) do
     object.cooked.gsub(/<div class='hide-for-guests'>(.*?)<\/div>/m, '<div class="hide-for-guests" data-hide-for-guests>$1</div>')
   end
 end
