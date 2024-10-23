25b2fdad998544281d83b5cbc1dc07ef657415ae
Unicorn9x <unicorn9x@yahoo.com.vn>
2024-10-23 19:51:07 +0700

    Add files via upload

---
+++ plugin.rb
@@ -0,0 +1,12 @@
+# name: discourse-custom-bbcode
+# about: A plugin to add custom BBCode for hiding content from guests.
+# version: 0.1
+# authors: Unicorn9x
+
+after_initialize do
+  # Register the custom BBCode
+  DiscourseMarkup::register_bbcode(
+    'hide-for-guests', 
+    '<div class="hide-for-guests">%{content}</div>'
+  )
+end
